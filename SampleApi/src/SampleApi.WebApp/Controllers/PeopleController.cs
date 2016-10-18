5e57c6028594ff9e910026e743674a7075d94426
rsantos <rsantos@braziliandevs.com>
2016-10-18 12:08:36 -0200

    firt commit

---
+++ SampleApi/src/SampleApi.WebApp/Controllers/PeopleController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SampleApi.WebApp.Models;
+
+namespace SampleApi.WebApp.Controllers
+{
+    [Route("api/people")]
+    public class PeopleController : Controller
+    {
+        private ICollection<Person> _people = new List<Person>();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(_people.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var person = _people.FirstOrDefault(x => x.Id == id);
+            if (person == null)
+            {
+                return NotFound($"Person with id {id} was not found");
+            }
+
+            return Json(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            person.Id = Guid.NewGuid();
+            _people.Add(person);
+
+            return Ok(person);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, [FromBody]Person updated)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var person = _people.FirstOrDefault(x => x.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _people.Remove(person);
+
+            updated.Id = id;
+            _people.Add(updated);
+
+            return Ok(updated);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var person = _people.FirstOrDefault(x => x.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _people.Remove(person);
+
+            return Ok();
+        }
+    }
+}
---
+++ SampleApi/src/SampleApi.WebApp/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApi.WebApp.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
