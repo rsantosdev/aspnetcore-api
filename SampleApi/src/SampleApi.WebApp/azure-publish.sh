5bd34442ad1bf9fbdaeb2fd12e2b97665ac126bd
rsantos <rsantos@braziliandevs.com>
2016-10-20 22:28:59 -0200

    Added exec command comment

--- SampleApi/src/SampleApi.WebApp/azure-publish.sh
+++ SampleApi/src/SampleApi.WebApp/azure-publish.sh
@@ -28,6 +28,12 @@ echo "  Uploading zip file"
 curl -H "If-Match: *" --user $AZURE_PUBLISH_USERNAME:$AZURE_PUBLISH_PASSWORD $AZURE_PUBLISH_URL/zip/site/wwwroot --upload-file ./bin/Debug/netcoreapp1.0/publish.zip
 echo "  Done Uploading zip file"
 
+# In case you need to execute commands on the web server, npm install for node.js apps for example
+# uncomment the following section
+# echo "  Sending npm install command"
+# curl -H "Content-Type: application/json" --user $AZURE_PUBLISH_USERNAME:$AZURE_PUBLISH_PASSWORD -X POST -d '{"command":"npm install","dir":"site\\wwwroot"}' $AZURE_PUBLISH_URL/command
+# echo "  Done Sending npm install command"
+
 echo "Deploy Finished!"
 
 # --------
